a549597c777b4a2d7923eee23a1208b6cb1d66cb
Emily Lim <emilylim@Emilys-MacBook-Air-4.local>
2019-10-28 10:35:45 -0700

    Added Nic's code

--- Em_code2019.R
+++ Em_code2019.R
@@ -148,6 +148,7 @@ summary(den)
 summary(int)
 summary(cara)
 
+
 par(mfrow = c(2,2))
 
 plot(cara)
@@ -201,10 +202,22 @@ summary(outI_g$fit)
 AIC(outI_g$fit,outII_g$fit)
 
 
-#### BOOTSTRAP
-set.seed(98765)
-outII_g_boot <- frair_boot(outII_g, start = NULL, strata=NULL, nboot=2000,
+####  OLD BOOTSTRAP
+#set.seed(98765)
+#outII_g_boot <- frair_boot(outII_g, start = NULL, strata=NULL, nboot=2000,
                            para=TRUE, ncores=NaN, WARN.ONLY=FALSE)
+
+
+# NEW bootstrap
+set.seed(309331)
+outII_g_boot <- frair_boot(outII_g, start = NULL, strata=green[,6], nboot=2000,
+                           para=TRUE, ncores=NaN, WARN.ONLY=FALSE)
+
+outII_emd_g_boot <- frair_boot(emdII_g, start = NULL, strata=green[,6], nboot=2000,
+                               para=TRUE, ncores=NaN, WARN.ONLY=FALSE)
+
+outII_g_boot
+outII_emd_g_boot
 
 # Illustrate bootlines
 plot(outII_g_boot, xlim=c(0,70), ylim = c(0, 40), type='n', main='All bootstrapped lines')
@@ -283,7 +296,7 @@ AIC(outI_r$fit,outII_r$fit, outIII_r$fit, outIII_r_2$fit)
 
 #### BOOTSTRAP 
 set.seed(604250)
-outIII_r_boot <- frair_boot(outIII_r, start = NULL, strata=NULL, nboot=2000,
+outIII_r_boot <- frair_boot(outIII_r, start = NULL, strata=red[,6], nboot=2000,
                             para=TRUE, ncores=NaN, WARN.ONLY=FALSE)
 
 # Illustrate bootlines
@@ -292,10 +305,16 @@ lines(outIII_r_boot, all_lines=TRUE)
 points(outIII_r_boot, pch=20)
 
 # Illustrate bootpolys
-plot(outIII_r_boot, xlim=c(0, 65), ylim = c(0, 70), type='n', main='Empirical 95 percent CI')
-drawpoly(outIII_r_boot, border = NA, col=hsv(0/6,0.4, 0.8, alpha = 0.3))
-points(outIII_r_boot, pch=20, col=hsv(0/6,0.4, 1))
-lines(outIII_r_boot, lwd = 3, all_lines=FALSE, col=hsv(0/6,0.4, 1, alpha = 1))
+plot(outIII_r_boot, xlim=c(0, 65), ylim = c(0, 40), type='n', main='Empirical 95 percent CI',
+     xlab = 'Prey Density', ylab = 'Prey Eaten',
+     cex.lab = 1.5,
+     font.lab = 2,
+     cex.axis = 1.2,
+     cex.main = 1.5)
+drawpoly(outIII_r_boot, border = NA, col=hsv(0/6,0.4, 0.8))
+points(outIII_r_boot, pch=20, col=hsv(0/6,0, 0))
+lines(outIII_r_boot, lwd = 2, all_lines=FALSE, col=hsv(0/6,0, 0, alpha = 1))
+
 
 ##### nlm to look at asymptote (analysis not in manuscript)
 red_asymp <- nls(eaten ~ SSasymp(density, Asym, R0, lrc), 
@@ -306,19 +325,125 @@ summary(red_asymp)
 
 # Functional response graph ---------------------------------
 
+plot(outIII_r_boot, xlim=c(0, 65), ylim = c(0, 40), type='n',
+     xlab = "Initial Oyster Density",
+     ylab="Oysters Consumed", 
+     cex.lab = 1.5,
+     font.lab = 2,
+     cex.axis = 1.2,
+     cex.main = 1.5)
+lines(outII_g_boot, lwd = 3, all_lines=FALSE, col=hsv(2.5/6,0.9, 0.5, alpha= 1), lty = 2)
+lines(outIII_r_boot, lwd = 3, all_lines=FALSE, col=hsv(6/6,0.9, 0.5, alpha = 1), lty = 1)
+drawpoly(outIII_r_boot, border = NA, col=hsv(6/6,0.6, 0.8, alpha = 0.4))
+drawpoly(outII_g_boot, border = NA, col=hsv(2.5/6,0.6, 0.8, alpha= 0.4))
+points(outIII_r_boot, pch=17, col=hsv(6/6,0.8, 0.5), cex = 1.4)
+points(outII_g_boot, pch=20,  col=hsv(2.5/6,0.8, 0.5, alpha= 1), cex = 1.4)
+legend(x = "topleft", legend = c("Green Crabs", "Red Rock Crabs"), 
+       col = c(hsv(2.5/6,0.9, 0.5, alpha= 1), c(hsv(6/6,0.9, 0.5))), 
+       lty = c(2, 1), cex = 1.3, pch = c(20, 17))
+
+
+# Comparing the distributions of the asymptotes of CP to CM ---------------
+green.densities <- c(64)
+
+boottest <- outII_g_boot$bootcoefs
+greenboot <- green.densities - lambertW0(boottest[, 1] * boottest[, 2] * green.densities * exp(-boottest[, 1] * (1 - boottest[, 2] * green.densities)))/(boottest[, 1] * boottest[, 2]) 
+
+red.densities <- c(64)
+boottest <- outIII_r_boot$bootcoefs
+redboota <- boottest[, 1] * red.densities^1.939
+redboot <- red.densities - lambertW0(redboota * boottest[, 3] * red.densities * exp(-redboota * (1 - boottest[, 3] * red.densities)))/(redboota * boottest[, 3])
+
+bootdisttest <- data.frame(green = greenboot, 
+                           red = redboot)
+
+ggplot(data = bootdisttest)+
+  geom_histogram(aes(x = green), alpha = 0.3, fill = 'green', colour = 'black', binwidth = 2)+
+  geom_histogram(aes(x = red), alpha = 0.3, fill = 'red', colour = 'black', binwidth = 2)+
+  labs(x = 'Expected oysters eaten with 64 starting density')
+
+library(fBasics)
+# Testing if there is a difference between the two asymptotes with kolmogorov-smirnov and t.tests
+ks2Test(bootdisttest$green, bootdisttest$red)
+t.test(bootdisttest$green, bootdisttest$red)
+
+# Everything I've tried so far results in different means of the two
+# species/different distributions that the two species are drawn from. t-test
+# suggests difference in mean maximum consumptive rate of ~7-9. I don't think I
+# can argue a non-statistically-significant difference without more data
+
+# This is just a test of how the effect of sample size on type 1 error when bootstrapping
+simfun <- function(n=6) {
+  x <- rnorm(n)
+  m.x <- mean(x)
+  s.x <- sd(x)
+  z <- m.x/(1/sqrt(n))
+  t <- m.x/(s.x/sqrt(n))
+  b <- replicate(2000, mean(sample(x, replace=TRUE)))
+  c( t=abs(t) > qt(0.975,n-1), z=abs(z) > qnorm(0.975),
+     z2 = abs(t) > qnorm(0.975), 
+     b= (0 < quantile(b, 0.025)) | (0 > quantile(b, 0.975))
+  )
+}
+
+out <- replicate(2000, simfun())
+rowMeans(out)
+
+# n = 3, nboot = 2000
+# t      z     z2 b.2.5% 
+# 0.0485 0.0500 0.1925 0.2545
+
+# n = 3, nboot = 200
+# t      z     z2 b.2.5% 
+# 0.060  0.040  0.175  0.270 
+
+# n = 6, nboot = 2000
+# t      z     z2 b.2.5% 
+# 0.0525 0.0510 0.1125 0.1430 
+
+
+
+# Bootstrapping for studentized and BCa intervals -------------------------
+
+CI_green <- confint(outII_g_boot)
+CI_red <- confint(outIII_r_boot)
+redframe <- data.frame(density = 0:64)
+redframe$a_up <- CI_red[['b']]$stud$upper * redframe$density^1.939
+redframe$a_lw <- CI_red[['b']]$stud$lower * redframe$density^1.939
+
+0:64 - lambertW0(redboota_up * CI_red[['h']]$stud$upper * 0:64 * exp(-redboota_up * (1 - CI_red[['b']]$stud$upper * 0:64)))/(redboota_up * CI_red[['b']]$stud$upper)
+
 
 # Illustrate bootpolys
-plot(outIII_r_boot, xlim=c(0, 65), ylim = c(0, 70), type='n',
-     main='Functional Response Curves')
-drawpoly(outIII_r_boot, border = NA, col=hsv(0/6,0.4, 0.8, alpha = 0.3))
-points(outIII_r_boot, pch=20, col=hsv(0/6,0.4, 1))
-lines(outIII_r_boot, lwd = 3, all_lines=FALSE, col=hsv(0/6,0.4, 1, alpha = 1))
+plot(outIII_r_boot, xlim=c(0, 65), ylim = c(0, 60), type='n', xlab = expression(paste(italic('Crassostrea gigas'), ' Density')), ylab = expression(paste('Number of ', italic('Crassostrea gigas'),' Consumed')))
+drawpoly(x = 0:64, 
+         upper = 0:64 - lambertW0(redboota_up * CI_red[['h']]$stud$lower * 0:64 * exp(-redboota_up * (1 - CI_red[['h']]$stud$lower * 0:64)))/(redboota_up * CI_red[['h']]$stud$lower),
+         lower = 0:64 - lambertW0(redboota_lw * CI_red[['h']]$stud$upper * 0:64 * exp(-redboota_lw * (1 - CI_red[['h']]$stud$upper * 0:64)))/(redboota_lw * CI_red[['h']]$stud$upper),
+         border = NA, col=hsv(0/6, 0.7, 0, alpha = 0.4))
+points(outIII_r_boot, pch=19, col=hsv(0/6, 0.7, 0, alpha = 0.5))
+lines(outIII_r_boot, lwd = 1, all_lines=FALSE)
 
 #add green
-drawpoly(outII_g_boot, border = NA, col=hsv(2/6,0.4, 0.8, alpha= 0.3))
-points(outII_g_boot, pch=20,  col=hsv(2/6,0.4, 0.8, alpha= 1))
-lines(outII_g_boot, lwd = 3, all_lines=FALSE, col=hsv(2/6,0.4, 0.8, alpha= 1))
+drawpoly(x = 0:64, 
+         upper = 0:64 - lambertW0(CI_green[['a']]$stud$upper * CI_green[['h']]$stud$upper * 0:64 * exp(-CI_green[['a']]$stud$upper * (1 - CI_green[['h']]$stud$upper * 0:64)))/(CI_green[['a']]$stud$upper * CI_green[['h']]$stud$upper),
+         lower = 0:64 - lambertW0(CI_green[['a']]$stud$lower * CI_green[['h']]$stud$lower * 0:64 * exp(-CI_green[['a']]$stud$lower * (1 - CI_green[['h']]$stud$lower * 0:64)))/(CI_green[['a']]$stud$lower * CI_green[['h']]$stud$lower),
+         border = NA, col=hsv(2/6, 1, 0, alpha = 0.2))
+points(outII_g_boot, pch=17,  col=hsv(2/6,1, 0, alpha= 0.5))
+lines(outII_g_boot, lwd = 1, all_lines=FALSE, lty = 2)
 
 # add legend
-legend(x = "topleft", legend = c("Green Crabs", "Red Rock Crabs"), col = c("green", "red"), 
-       lty = 1, cex = 1)
+legend(x = "topleft", legend = c(expression(italic("Carcinus maenas")), expression(italic("Cancer productus"))), pch = c(17, 19), 
+       lty = c(2, 1), cex = 1, fill = c(hsv(2/6, 1, 0, 0.2), hsv(0/6, 1, 0, 0.4)))
+
+
+# Number eaten per cm cheliped --------------------------------------------
+
+cheliped <- read.csv('functional_response_cheliped.csv', header = T)
+greenchel <- cheliped[cheliped$species == 'green',]
+redchel <- cheliped[cheliped$species == 'red',]
+
+par(mfrow = c(1, 1))
+plot(cheliped$density_per_cm_round[cheliped$species == 'green'], cheliped$eaten_per_cm[cheliped$species == 'green'], pch = 19, col = 'forestgreen',
+     xlab = 'Initial prey density', ylab = 'Number of prey eaten per cm cheliped height')
+points(cheliped$density_per_cm_round[cheliped$species == 'red'], cheliped$eaten_per_cm[cheliped$species == 'red'], pch = 17, col = 'firebrick')
+legend('topleft', col = c('forestgreen', 'firebrick'), pch = c(19, 17), legend = c('Carcinus maenas', 'Cancer productus'))
